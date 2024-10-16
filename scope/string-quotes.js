85e948a857a8ab03ebfa1f195d95cc3fa539716f
Lena Man <manlena01@gmail.com>
2024-10-16 15:33:08 -0400

    string

---
+++ scope/string-quotes.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let quote1 = "In Texas, we use the word ya'll often"; 
+let quote2 = 'Big Tex (at the State Fair) says "Howdy"';
+
+let fullName = "Richardo Bustamante";
+console.log(fullName.length);
---
+++ string-index.js
@@ -0,0 +1,11 @@
+"use strict";
+let fullName = "Mark Westley";
+
+// let firstLetter = fullName[9];
+// console.log(firstLetter);
+
+let positionOfTheSpace = fullName.indexOf(" ")
+console.log(positionOfTheSpace);
+
+let lastName = fullName.substring(positionOfTheSpace);
+console.log(lastName);
